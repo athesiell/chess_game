550f7e5991180b08aab45ef27f85a559d8b19db9
Aidar <ar.ibraev98@gmail.com>
2023-12-16 13:48:09 +0600

    Update board file and its methods

--- lib/board.rb
+++ lib/board.rb
@@ -8,11 +8,12 @@ require_relative 'rook'
 
 class Board
   attr_reader :board
+  attr_accessor :player1_pieces, :player2_pieces
 
   def initialize
     @board = Array.new(8) { Array.new(8) }
-    place_pieces_white
-    place_pieces_black
+    @player1_pieces = place_pieces_white
+    @player2_pieces = place_pieces_black
   end
 
   def display_board
@@ -34,31 +35,31 @@ class Board
 
   def place_pieces_white
     (0..7).each do |i|
-      @board[1][i] = Pawn.new(:white)
+      @board[1][i] = Pawn.new([1][i], :white)
     end
-    @board[0][0] = Rook.new(:white)
-    @board[0][7] = Rook.new(:white)
-    @board[0][1] = Knight.new(:white)
-    @board[0][6] = Knight.new(:white)
-    @board[0][2] = Bishop.new(:white)
-    @board[0][5] = Bishop.new(:white)
-    @board[0][3] = Queen.new(:white)
-    @board[0][4] = King.new(:white)
-  end
+    @board[0][0] = Rook.new([0][0], :white)
+    @board[0][7] = Rook.new([0][7], :white)
+    @board[0][1] = Knight.new([0][1], :white)
+    @board[0][6] = Knight.new([0][6], :white)
+    @board[0][2] = Bishop.new([0][2], :white)
+    @board[0][5] = Bishop.new([0][5], :white)
+    @board[0][3] = Queen.new([0][3], :white)
+    @board[0][4] = King.new([0][4], :white)
+ end
 
-  def place_pieces_black
-    (0..7).each do |i|
-      @board[6][i] = Pawn.new(:black)
-    end
-    @board[7][0] = Rook.new(:black)
-    @board[7][7] = Rook.new(:black)
-    @board[7][1] = Knight.new(:black)
-    @board[7][6] = Knight.new(:black)
-    @board[7][2] = Bishop.new(:black)
-    @board[7][5] = Bishop.new(:black)
-    @board[7][3] = Queen.new(:black)
-    @board[7][4] = King.new(:black)
+ def place_pieces_black
+  (0..7).each do |i|
+    @board[6][i] = Pawn.new([6][i], :black)
   end
+  @board[7][0] = Rook.new([7][0], :black)
+  @board[7][7] = Rook.new([7][7], :black)
+  @board[7][1] = Knight.new([7][1], :black)
+  @board[7][6] = Knight.new([7][6], :black)
+  @board[7][2] = Bishop.new([7][2], :black)
+  @board[7][5] = Bishop.new([7][5], :black)
+  @board[7][3] = Queen.new([7][3], :black)
+  @board[7][4] = King.new([7][4], :black)
+ end
 
   def update_board(location, piece)
     return 'Location should be array with 2 elements in it' unless location.is_a?(Array) && location.length == 2
