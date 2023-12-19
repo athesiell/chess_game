f924795914bc9f3c1fc2c1fe74ac66b9d398a4ca
Aidar <ar.ibraev98@gmail.com>
2023-12-19 21:23:15 +0600

    Add new methods for piece to move, remove old ones

--- lib/board.rb
+++ lib/board.rb
@@ -28,58 +28,79 @@ class Board
         end
       end
     puts "| #{index + 1}" 
-    puts ' ' + '-' * 35 if index > 0
+    puts ' ' + '-' * 33 if index > 0
     end
     puts '   a   b   c   d   e   f   g   h'
   end
 
   def place_pieces_white
-    (0..7).each do |i|
-      @board[1][i] = Pawn.new([1][i], :white)
+    8.times do |i|
+      self[[1, i]] = Pawn.new([1, i], :white, self)
     end
-    @board[0][0] = Rook.new([0][0], :white)
-    @board[0][7] = Rook.new([0][7], :white)
-    @board[0][1] = Knight.new([0][1], :white)
-    @board[0][6] = Knight.new([0][6], :white)
-    @board[0][2] = Bishop.new([0][2], :white)
-    @board[0][5] = Bishop.new([0][5], :white)
-    @board[0][3] = Queen.new([0][3], :white)
-    @board[0][4] = King.new([0][4], :white)
- end
-
- def place_pieces_black
-  (0..7).each do |i|
-    @board[6][i] = Pawn.new([6][i], :black)
-  end
-  @board[7][0] = Rook.new([7][0], :black)
-  @board[7][7] = Rook.new([7][7], :black)
-  @board[7][1] = Knight.new([7][1], :black)
-  @board[7][6] = Knight.new([7][6], :black)
-  @board[7][2] = Bishop.new([7][2], :black)
-  @board[7][5] = Bishop.new([7][5], :black)
-  @board[7][3] = Queen.new([7][3], :black)
-  @board[7][4] = King.new([7][4], :black)
- end
-
-  def update_board(location, piece)
-    return 'Location should be array with 2 elements in it' unless location.is_a?(Array) && location.length == 2
-    row = location[0]
-    column = location[1]
-    return 'Invalid move' unless row >= 0 && row <= 7 && column >=0 && column <= 7
-    @board[row][column] = piece.to_s
-    display_board
+    self[[0, 0]] = Rook.new([0, 0], :white, self)
+    self[[0, 7]] = Rook.new([0, 7], :white, self)
+    self[[0, 1]] = Knight.new([0, 1], :white, self)
+    self[[0, 6]] = Knight.new([0, 6], :white, self)
+    self[[0, 2]] = Bishop.new([0, 2], :white, self)
+    self[[0, 5]] = Bishop.new([0, 5], :white, self)
+    self[[0, 3]] = Queen.new([0, 3], :white, self)
+    self[[0, 4]] = King.new([0, 4], :white, self)
   end
 
-  def reset_piece(location)
-    return 'Location should be array with 2 elements in it' unless location.is_a?(Array) && location.length == 2
-    row = location[0]
-    column = location[1]
-    return 'Invalid move' unless row >= 0 && row <= 7 && column >= 0 && column <= 7
-    @board[row][column] = nil
-    display_board
+  def place_pieces_black
+    8.times do |i|
+      self[[6, i]] = Pawn.new([6, i], :black, self)
+    end
+    self[[7, 0]] = Rook.new([7, 0], :black, self)
+    self[[7, 7]] = Rook.new([7, 7], :black, self)
+    self[[7, 1]] = Knight.new([7, 1], :black, self)
+    self[[7, 6]] = Knight.new([7, 6], :black, self)
+    self[[7, 2]] = Bishop.new([7, 2], :black, self)
+    self[[7, 5]] = Bishop.new([7, 5], :black, self)
+    self[[7, 3]] = Queen.new([7, 3], :black, self)
+    self[[7, 4]] = King.new([7, 4], :black, self)
+  end
+
+  def []=(location, piece)
+    row, col = location
+    board[row][col] = piece
+  end
+
+  def [](location)
+    row, col = location
+    board[row][col]
+  end
+
+  def empty?(location)
+    row, col = location
+    board[row][col].nil?
+  end
+
+  def in_boards?(location)
+    row, col = location
+    row >= 0 && row < 8 && col >= 0 && col < 8
+  end
+
+  def translate_coordinates(coord)
+    col = coord[0].ord - 'a'.ord
+    row = coord[1].to_i - 1
+    [row, col]
+  end
+
+  def move_piece(from, to)
+    from_row, from_col = translate_coordinates(from)
+    to_row, to_col = translate_coordinates(to)
+
+    piece = board[from_row][from_col]
+    if !piece.available_moves.include?([to_row, to_col])
+      puts "Invalid move for #{piece.class} piece, please choose again"
+      return
+    else
+      board[to_row][to_col] = piece
+      board[from_row][from_col] = nil
+      puts "Moved #{piece.class} from #{from} to #{to}"
+    end
   end
 end
 
 
-board = Board.new
-board.display_board
