91e2f85247a077f33eab9d1a92a793432f8da7c4
Aidar <ar.ibraev98@gmail.com>
2023-12-20 23:28:18 +0600

    Add check for enemy?

---
+++ lib/pieces/stepable.rb
@@ -0,0 +1,18 @@
+module Stepable
+  def available_moves
+    moves = []
+
+    move_directions.each do |(row_dir, col_dir)|
+      cur_row, cur_col = position
+
+      cur_row += row_dir
+      cur_col += col_dir
+      location = [cur_row, cur_col]
+      next if !board.in_boards?(location)
+      if board.empty?(location) || enemy?(location)
+        moves << location
+      end
+    end
+    moves
+  end
+end
