b289d5ec31da7f45b8dfbba7d423d499abb236c1
Aidar <ar.ibraev98@gmail.com>
2023-12-22 21:46:12 +0600

    Add instructions

--- main.rb
+++ main.rb
@@ -1,5 +1,21 @@
 require_relative './lib/game'
 
+puts <<~HEREDOC
 
+    CHESS GAME
+
+    Welcome to the CHESS!
+
+    The goal is to checkmate enemy's King piece
+
+    Wins the player who manages to checkmate it
+
+    To pick the chess piece you can type letter and the number that piece is standing on
+
+    To move the piece just type the letter and the number of the cell
+
+    Good Luck and Have Fun!
+
+    HEREDOC
 game = Game.new
 game.play
